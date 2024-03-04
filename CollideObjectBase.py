4e822c88c0f360f0a981fe29c442caad27a8739c
thomas3761 <102927056+thomas3761@users.noreply.github.com>
2024-03-04 13:37:03 -0500

    add the missing class starting over from here

--- CollideObjectBase.py
+++ CollideObjectBase.py
@@ -14,14 +14,20 @@ class PlacedObject(PandaNode):
 
 class CollidableObject(PlacedObject):
 
-    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, colPositionVec: Vec3, colRadius: float):
-        super().__init__(loader, modelPath, parentNode, nodeName)  # Provide dummy values for capsule parameters
-        self.collisionNode.node().addSolid(CollisionCapsule(colPositionVec, colRadius))
-        self.collisionNode.show()
+    def __init__(self, Loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str):
+        super(CollidableObject, self).__init__(Loader, modelPath, parentNode, nodeName)
+
+        self.collisionNode = self.modelNode.attachNewNode(CollisionNode(nodeName + '_cNode'))
 
 class InverseSphereCollideObject(CollidableObject):
 
-    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, colPositionVec: Vec3, colRadius: float):
-        super().__init__(loader, modelPath, parentNode, nodeName, 0, 0, 0, 0, 0, 0, 0)  # Provide dummy values for capsule parameters
+    def __init__(self, Loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, colPositionVec: Vec3, colRadius: float):
+        super(InverseSphereCollideObject, self).__init__(Loader, modelPath, parentNode, nodeName)
         self.collisionNode.node().addSolid(CollisionCapsule(colPositionVec, colRadius))
+        #self.collisionNode.show()
+
+class CollisionCapsuleObject(CollidableObject):
+    def __init__(self, Loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str,ax: float, ay: float, az: float, bx: float, by: float, bz: float, r: float):
+        super(CollidableObject, self).__init__(Loader, modelPath, parentNode, nodeName)
+        self.collisionNode.node().addSolid(CollisionCapsule(ax, ay, bx, by, bz, r))
         self.collisionNode.show()
